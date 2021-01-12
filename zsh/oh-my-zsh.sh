8ce6a8de191cfbe1d17cd76a3f963ca38233054a
Benjamin Bolte <ben@bolte.cc>
2021-01-11 17:44:20 -0800

    update zsh thing

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -1,6 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
-if [[ ! -d ~/.oh-my-zsh ]]; then
+if [ ! -d "~/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
         --keep-zshrc \
         --unattended

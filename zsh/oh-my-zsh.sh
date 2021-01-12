f15e38b1aefd01324058166ca2b5ebeaba6bb0a4
Benjamin Bolte <ben@bolte.cc>
2021-01-11 20:46:32 -0500

    zsh thing

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d "~/.oh-my-zsh" ]; then
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" \
         --keep-zshrc \
         --unattended

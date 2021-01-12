5a5403d14d960745c315407cda96b0adee430da0
Benjamin Bolte <ben@bolte.cc>
2021-01-11 18:08:39 -0800

    bug fix

--- bash/autocomplete.bash
+++ bash/autocomplete.bash
@@ -30,7 +30,7 @@ _tcomplete() {
 
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    opts="$(find $TMP_SCRIPT_ROOT -type f | cut -c$((${#TMP_SCRIPT_ROOT}+2))- | paste -sd " " -)"
+    opts="$(find $TMP_SCRIPT_ROOT -type f | cut -c$((${#TMP_SCRIPT_ROOT}+1))- | paste -sd " " -)"
 
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     return 0

598bcec2f528674e3f9fce5b41fe8075bd995b93
Benjamin Bolte <ben@bolte.cc>
2021-02-10 18:00:50 -0500

    remove info stuff

--- shell/info.sh
+++ shell/info.sh
@@ -1,15 +1,16 @@
 # conda
-if [[ -f ~/.conda/environments.txt ]]; then
-    echo -e "\033[1;32m----- Conda Environments -----\033[0m"
-    cat ~/.conda/environments.txt | xargs -L1 basename
-    echo -e "\033[1;32m------------------------------\033[0m"
-fi
+# if [[ -f ~/.conda/environments.txt ]]; then
+#     echo -e "\033[1;32m----- Conda Environments -----\033[0m"
+#     cat ~/.conda/environments.txt | xargs -L1 basename
+#     echo -e "\033[1;32m------------------------------\033[0m"
+# fi
 
 # tmux
-if [[ -n $TMUX ]] && [[ $TMUX != /tmp/* ]]; then
-    echo -e "\033[1;31m----- TMUX session: $(tmux display-message -p '#S') -----\033[0m"
-elif [[ ! -n $TMUX ]] && [[ $(tmux ls 2> /dev/null) ]]; then
-    echo -e "\033[1;31m----- Open tmux sessions -----\033[0m"
-    tmux ls
-    echo -e "\033[1;31m------------------------------\033[0m"
-fi
+# if [[ -n $TMUX ]]; then
+#     echo -e "\033[1;31m----- TMUX session: $(tmux display-message -p '#S') -----\033[0m"
+# elif [[ ! -n $TMUX ]] && [[ $(tmux ls 2> /dev/null) ]]; then
+#     echo -e "\033[1;31m----- Open tmux sessions -----\033[0m"
+#     tmux ls
+#     echo -e "\033[1;31m------------------------------\033[0m"
+# fi
+

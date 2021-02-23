0ade9778f272f77534bef96304bac16e859a2bcb
Benjamin Bolte <ben@bolte.cc>
2021-02-22 20:34:13 -0800

    finally fix left right movement issue

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -29,27 +29,34 @@ export EDITOR=vim
 
 # Use vim style line editing in zsh
 bindkey -v
+
 # Use vim style navigation keys in menu completion
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
+
 # fight me
 bindkey jk vi-cmd-mode
 bindkey kj vi-cmd-mode
+
 # emacs things in insert mode
 bindkey "^P" up-line-or-history
 bindkey "^N" down-line-or-history
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
+
 # Movement
 bindkey -a 'gg' beginning-of-buffer-or-history
 bindkey -a 'G' end-of-buffer-or-history
+
 # Undo
 bindkey -a 'u' undo
 bindkey -a '^R' redo
+
 # Edit line
 bindkey -a '^V' edit-command-line
+
 # Backspace
 bindkey '^?' backward-delete-char
 bindkey '^H' backward-delete-char
@@ -59,3 +66,16 @@ bindkey "^R" history-incremental-search-backward
 
 # Disable shell builtins
 disable r
+
+# Move left and right
+case `uname` in
+    Darwin)
+        bindkey "^[^[[C" forward-word
+        bindkey "^[^[[D" backward-word
+    ;;
+    Linux)
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+    ;;
+esac
+

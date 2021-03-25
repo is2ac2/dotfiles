22a24b5147587602c6ee1114ec7dc87e14b38461
Benjamin Bolte <ben@bolte.cc>
2021-03-25 00:28:25 -0500

    fix mac compatibility

--- cron/delete_old_dates.sh
+++ cron/delete_old_dates.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
-start_date=$(date --date '90 days ago' +'%Y-%m-%d')
-end_date=$(date --date '30 days ago' +'%Y-%m-%d')
+# Get date range, handling different operating systems.
+case $OSTYPE in
+    "darwin"*)
+        start_date=$(date -v-90d +'%Y-%m-%d')
+        end_date=$(date -v-30d +'%Y-%m-%d')
+        ;;
+    "linux-gnu"*)
+        start_date=$(date --date '90 days ago' +'%Y-%m-%d')
+        end_date=$(date --date '30 days ago' +'%Y-%m-%d')
+        ;;
+    *)
+        echo "OS type not supported: $OSTYPE"
+        return
+        ;;
+esac
 
 # Deletes logs older than 30 days.
-python3 ${HOME}/.scripts/manage_dates \
-    ~/logs/ \
-    -s $start_date \
-    -e $end_date \
-    -a delete
+if [[ -d ${HOME}/logs ]]; then
+    python3 ${HOME}/.scripts/manage_dates \
+        ${HOME}/logs/ \
+        -s $start_date \
+        -e $end_date \
+        -a delete
+fi
 
 # Deletes eval older than 30 days.
-python3 ${HOME}/.scripts/manage_dates \
-    ~/eval/ \
-    -s $start_date \
-    -e $end_date \
-    -a delete
+if [[ -d ${HOME}/eval ]]; then
+    python3 ${HOME}/.scripts/manage_dates \
+        ${HOME}/eval/ \
+        -s $start_date \
+        -e $end_date \
+        -a delete
+fi
 

e52890e801d13cc4621bea1e4d4c3ec2120e1ab9
Benjamin Bolte <ben@bolte.cc>
2021-03-27 00:27:10 -0400

    turn pdb back off

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -6,7 +6,5 @@ c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = [
     '%autoreload 2',
     '%matplotlib inline',
-    '%pdb on',
 ]
-c.TerminalInteractiveShell.pdb = True
 

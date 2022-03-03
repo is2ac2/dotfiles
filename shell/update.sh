0d588410be4c49b1fa7693793056eb17805342c7
Benjamin Bolte <bbolte@tesla.com>
2022-03-02 18:14:46 -0800

    remove hiding thing

--- shell/update.sh
+++ shell/update.sh
@@ -7,7 +7,7 @@ cd ${HOME}/.dotfiles
 if [[ $(git status --porcelain) ]]; then
     echo -e "\033[1;31m----- Uncommitted Dotfile changes -----\033[0m"
 else
-    git pull origin master 1> /dev/null
+    git pull origin master --quiet
 fi
 
 cd ${old_dir}

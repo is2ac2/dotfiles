d634697eb8dbf995a29440e2d4e85d16ee327780
Benjamin Bolte <bbolte@tesla.com>
2022-03-10 12:52:07 -0500

    add better warning message

--- shell/update.sh
+++ shell/update.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+warn-big-text() {
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: warn-big-text <text>"
+    else
+        local text="WARNING: $1"; shift
+        printf '%*s\n' "${#text}" '' | tr ' ' '!'
+        printf "${RED}${text}${NC}\n"
+        printf '%*s\n' "${#text}" '' | tr ' ' '!'
+    fi
+}
+
 pull_dotfiles() {
     local old_dir=$(pwd)
     cd ${HOME}/.dotfiles
     if [[ $(git status --porcelain) ]]; then
-        echo -e "\033[1;31m----- Uncommitted Dotfile changes -----\033[0m"
+        warn-big-text 'Uncommitted Dotfile changes'
     else
         git pull origin master --quiet
     fi

3300dbe1419e5a1b979e05d04bb6f471c7e546bc
Benjamin Bolte <ben@bolte.cc>
2023-04-09 05:39:28 -0400

    add back tmux things

--- shell/info.sh
+++ shell/info.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# conda
+# Conda environments
 if [[ -f ${HOME}/.conda/environments.txt ]] && [[ ! -z $(grep '[^[:space:]]' "${HOME}/.conda/environments.txt")  ]]; then
     cat ${HOME}/.conda/environments.txt | sort | while read line; do echo -n -e "[\033[1;32m$(basename $line)\033[0m] "; done
+    echo -n -e "\n"
+fi
+
+# TMUX active sessions
+if [[ ! -z $(tmux ls 2>/dev/null) ]]; then
+    tmux ls 2>/dev/null | while read line; do echo -n -e "[\033[1;35m$(echo $line | cut -d: -f1)\033[0m] "; done
     echo -n -e "\n"
 fi

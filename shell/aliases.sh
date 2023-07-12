adc6137c5b802b2cc4b3b1451e3a143b2c5183de
Benjamin Bolte <ben@bolte.cc>
2023-07-11 21:48:14 -0600

    aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -552,6 +552,7 @@ aptu() {
 export NVM_DIR="$HOME/.nvm"
 load-nvm() {
     unalias nvm 2> /dev/null
+    unalias npm 2> /dev/null
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }

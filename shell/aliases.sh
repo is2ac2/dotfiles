e80672b8d097cab370ac2557b7495fab01db9cba
Benjamin Bolte <ben@bolte.cc>
2023-07-15 10:59:23 -0400

    re-alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -556,9 +556,8 @@ load-nvm() {
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
-
-alias nvm='load-nvm && \nvm'
-alias npm='load-nvm && \npm'
+alias nvm='echo "Run load-nvm first"'
+alias npm='echo "Run load-nvm first"'
 
 # Ruby
 load-ruby() {
